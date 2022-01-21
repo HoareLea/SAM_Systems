c56b32725400d204ad13a968b1f71aea6d9e1191
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-21 16:19:03 +0000

    PlantRoom and SystemModel Added

---
+++ SAM_Systems/SAM.Core.Systems/Classes/PlantRoom.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Core.Systems
+{
+    public class PlantRoom : SAMObject
+    {
+
+        public PlantRoom(PlantRoom plantRoom)
+            :base(plantRoom)
+        {
+
+        }
+
+        public PlantRoom(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public PlantRoom(string name)
+            : base(name)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            return base.FromJObject(jObject);
+        }
+
+        public override JObject ToJObject()
+        {
+            return base.ToJObject();
+        }
+
+    }
+}
