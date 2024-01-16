7014fce24a5cb8a6f87a6e97046b291080df88f6
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-01-16 16:57:29 +0100

    SystemAirJunction Implemented

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemAirJunction.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core.Systems;
+
+namespace SAM.Analytical.Systems
+{
+    public class SystemAirJunction : SystemJunction<AirSystem>
+    {
+        public SystemAirJunction()
+            : base()
+        {
+
+        }
+
+        public SystemAirJunction(SystemAirJunction systemAirJunction)
+            : base(systemAirJunction)
+        {
+
+        }
+
+        public SystemAirJunction(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            return base.FromJObject(jObject);
+        }
+
+        public override JObject ToJObject()
+        {
+            return base.ToJObject();
+        }
+    }
+}
--- SAM_Systems/SAM.Core.Systems/Classes/SystemJunction.cs
+++ SAM_Systems/SAM.Core.Systems/Classes/SystemJunction.cs
@@ -2,8 +2,8 @@
 
 namespace SAM.Core.Systems
 {
-    public class SystemJunction : SystemComponent
-    { 
+    public class SystemJunction : SystemJunction<ISystem>
+    {
         public SystemJunction(SystemJunction systemJunction)
             : base(systemJunction)
         {
@@ -21,6 +21,27 @@ namespace SAM.Core.Systems
         {
 
         }
+    }
+
+    public abstract class SystemJunction<T> : SystemComponent where T : ISystem
+    { 
+        public SystemJunction(SystemJunction<T> systemJunction)
+            : base(systemJunction)
+        {
+
+        }
+
+        public SystemJunction(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public SystemJunction()
+            : base(typeof(SystemJunction<T>).Name)
+        {
+
+        }
 
         public SystemJunction(string name)
             : base(name)
@@ -34,8 +55,8 @@ namespace SAM.Core.Systems
             {
                 return Create.SystemConnectorManager
                 (
-                    Create.SystemConnector<ISystem>(Direction.In, 1),
-                    Create.SystemConnector<ISystem>(Direction.Out, 1)
+                    Create.SystemConnector<T>(Direction.In, 1),
+                    Create.SystemConnector<T>(Direction.Out, 1)
                 );
             }
         }
