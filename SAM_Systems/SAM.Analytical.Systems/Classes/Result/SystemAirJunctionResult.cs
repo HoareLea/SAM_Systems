2d8a8fabebdd6f11692f2cae9ff7f0e72c99769c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-01-16 17:20:08 +0100

    SystemAirJunctionResult Implemented

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/Result/SystemAirJunctionResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+using SAM.Core.Systems;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Systems
+{
+    public class SystemAirJunctionResult : SystemIndexedDoublesResult, ISystemComponentResult
+    { 
+        public SystemAirJunctionResult(string uniqueId, string name, string source, Dictionary<AirJunctionDataType, IndexedDoubles> dictionary)
+            : base(uniqueId, name, source, Core.Systems.Query.Dictionary(dictionary))
+        {
+        }
+
+        public SystemAirJunctionResult(JObject jObject)
+            : base(jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public SystemAirJunctionResult(SystemAirJunctionResult systemAirJunctionResult)
+            : base(systemAirJunctionResult)
+        {
+
+        }
+    }
+}
