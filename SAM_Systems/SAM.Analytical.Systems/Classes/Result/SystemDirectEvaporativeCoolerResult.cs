8eb26f19828c6c5aadf430cde47600e90dd635fa
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-01-17 13:22:22 +0100

    SystemDirectEvaporativeCooler Implemented

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/Result/SystemDirectEvaporativeCoolerResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+using SAM.Core.Systems;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Systems
+{
+    public class SystemDirectEvaporativeCoolerResult : SystemIndexedDoublesResult, ISystemComponentResult
+    { 
+        public SystemDirectEvaporativeCoolerResult(string uniqueId, string name, string source, Dictionary<DirectEvaporativeCoolerDataType, IndexedDoubles> dictionary)
+            : base(uniqueId, name, source, Core.Systems.Query.Dictionary(dictionary))
+        {
+        }
+
+        public SystemDirectEvaporativeCoolerResult(JObject jObject)
+            : base(jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public SystemDirectEvaporativeCoolerResult(SystemDirectEvaporativeCoolerResult systemDirectEvaporativeCoolerResult)
+            : base(systemDirectEvaporativeCoolerResult)
+        {
+
+        }
+    }
+}
---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemDirectEvaporativeCooler.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SAM.Analytical.Systems
+{
+    public class SystemDirectEvaporativeCooler : SystemHumidifier
+    {
+        public SystemDirectEvaporativeCooler(string name)
+            : base(name)
+        {
+
+        }
+
+        public SystemDirectEvaporativeCooler(SystemDirectEvaporativeCooler systemDirectEvaporativeCooler)
+            : base(systemDirectEvaporativeCooler)
+        {
+
+        }
+
+        public SystemDirectEvaporativeCooler(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            return base.FromJObject(jObject);
+        }
+
+        public override JObject ToJObject()
+        {
+            return base.ToJObject();
+        }
+    }
+}
