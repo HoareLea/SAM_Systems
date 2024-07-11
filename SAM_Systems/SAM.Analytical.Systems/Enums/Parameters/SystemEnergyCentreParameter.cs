1f50adeca4828f619a5c4e2807f3a312143b7c48
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-07-11 18:52:08 +0200

    SystemEnergyCentre Parameters Added

---
+++ SAM_Systems/SAM.Analytical.Systems/Enums/Parameters/SystemEnergyCentreParameter.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using SAM.Core.Attributes;
+using SAM.Core.Systems;
+
+namespace SAM.Analytical.Systems
+{
+    [AssociatedTypes(typeof(SystemEnergyCentre)), Description("SystemEnergyCentre Parameter")]
+    public enum SystemEnergyCentreParameter
+    {
+        [ParameterProperties("Annual Total Consumption", "Annual Total Consumption [kWh]"), ParameterValue(Core.ParameterType.Double)] AnnualTotalConsumption,
+        [ParameterProperties("Annual CO2 Emission", "Annual CO2 Emission [kg]"), ParameterValue(Core.ParameterType.Double)] AnnualCO2Emission,
+        [ParameterProperties("Annual Cost", "Annual Cost"), ParameterValue(Core.ParameterType.Double)] AnnualCost,
+        [ParameterProperties("Annual Unmet Hours", "Annual Unmet Hours [h]"), ParameterValue(Core.ParameterType.Double)] AnnualUnmetHours,
+    }
+}
