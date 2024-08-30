d749bf70e2f307fac2c5e9b988efa28af8968e3a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-08-30 12:35:18 +0200

    ProfileSetpoint and RangeSetpoint Implemented

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/Setpoint.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+
+namespace SAM.Analytical.Systems
+{
+    public abstract class Setpoint : ISetpoint
+    {
+        public Setpoint()
+        {
+
+        }
+
+        public Setpoint(Setpoint setpoint)
+        {
+
+        }
+
+        public Setpoint(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public virtual bool FromJObject(JObject jObject)
+        {
+            if (jObject == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public virtual JObject ToJObject()
+        {
+            JObject result = new JObject();
+            result.Add("_type", Core.Query.FullTypeName(this));
+
+            return result;
+        }
+    }
+}
---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core.Systems;
+
+namespace SAM.Analytical.Systems
+{
+    public abstract class SystemController : SystemComponent
+    {
+        public SystemController(string name)
+            : base(name)
+        {
+
+        }
+
+        public SystemController(SystemController systemController)
+            : base(systemController)
+        {
+            if (systemController != null)
+            {
+
+            }
+        }
+
+        public SystemController(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            return true;
+        }
+
+        public override JObject ToJObject()
+        {
+            JObject result = base.ToJObject();
+            if (result == null)
+            {
+                return null;
+            }
+
+            return result;
+        }
+    }
+}
