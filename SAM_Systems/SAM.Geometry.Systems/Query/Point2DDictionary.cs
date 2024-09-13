656526583686e038e0b9012aa842018bbf58e316
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-09-13 14:44:19 +0200

    Point2DDictionary Query Implemented

---
+++ SAM_Systems/SAM.Geometry.Systems/Query/Point2DDictionary.cs
@@ -0,0 +1,38 @@
+﻿using SAM.Core;
+using SAM.Core.Systems;
+using SAM.Geometry.Planar;
+using System.Collections.Generic;
+
+namespace SAM.Geometry.Systems
+{
+    public static partial class Query
+    {
+        public static Dictionary<int, Point2D> Point2DDictionary(this SystemPlantRoom systemPlantRoom, IDisplaySystemObject<SystemGeometryInstance> displaySystemObject, SystemType systemType, ConnectorStatus connectorStatus = ConnectorStatus.Undefined, Direction? direction = null)
+        {
+            if(systemPlantRoom == null || displaySystemObject == null || systemType == null)
+            {
+                return null;
+            }
+
+            Dictionary<int, int> dictionary = IndexDictionary(systemPlantRoom, displaySystemObject, systemType, connectorStatus, direction);
+            if (dictionary == null || dictionary.Count == 0)
+            {
+                return null;
+            }
+
+            Dictionary<int, Point2D> result = new Dictionary<int, Point2D>();
+            foreach(KeyValuePair<int, int> keyValuePair in dictionary)
+            {
+                Point2D point2D = (displaySystemObject as dynamic).SystemGeometry.GetPoint2D(keyValuePair.Value);
+                if (point2D == null)
+                {
+                    continue;
+                }
+
+                result[keyValuePair.Key] = point2D;
+            }
+
+            return result;
+        }
+    }
+}
