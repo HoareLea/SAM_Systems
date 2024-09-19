b6585138344b0df82226f13e1f9fb78132e651e2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-09-19 15:44:07 +0200

    SystemEnergyCentreGroup Implemented

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/Result/SystemEnergyCentreGroup.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+
+namespace SAM.Analytical.Systems
+{
+    public class SystemEnergyCentreGroup : IndexedDoubles
+    {
+        private string category;
+        private string name;
+
+        public SystemEnergyCentreGroup(JObject jObject)
+            : base(jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public SystemEnergyCentreGroup(SystemEnergyCentreGroup systemEnergyCentreGroup)
+            : base(systemEnergyCentreGroup)
+        {
+            if (systemEnergyCentreGroup != null)
+            {
+                category = systemEnergyCentreGroup.category;
+            }
+        }
+
+        public SystemEnergyCentreGroup(string name, string category, IndexedDoubles indexedDoubles)
+            : base(indexedDoubles)
+        {
+            this.category = category;
+            this.name = name;
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            if (jObject.ContainsKey("Category"))
+            {
+                category = jObject.Value<string>("Category");
+            }
+
+            if (jObject.ContainsKey("Name"))
+            {
+                name = jObject.Value<string>("Name");
+            }
+
+            return result;
+        }
+
+        public override JObject ToJObject()
+        {
+            JObject jObject = base.ToJObject();
+            if (jObject == null)
+            {
+                return null;
+            }
+
+            if (category != null)
+            {
+                jObject.Add("Category", category);
+            }
+
+            if (name != null)
+            {
+                jObject.Add("Name", name);
+            }
+
+            return jObject;
+        }
+    }
+}
---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/Result/SystemEnergyCentreResult.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+using SAM.Core.Systems;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Systems
+{
+    public class SystemEnergyCentreResult : Result, ISystemResult
+    {
+        private List<SystemEnergyCentreGroup> systemEnergyCentreGroups = new List<SystemEnergyCentreGroup>();
+        
+        public SystemEnergyCentreDataType SystemEnergyCentreDataType { private set; get; }
+
+        public SystemEnergyCentreResult(JObject jObject)
+            : base(jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public SystemEnergyCentreResult(SystemEnergyCentreResult systemEnergyCentreResult)
+            : base(systemEnergyCentreResult)
+        {
+            if (systemEnergyCentreResult != null)
+            {
+                SystemEnergyCentreDataType = systemEnergyCentreResult.SystemEnergyCentreDataType;
+                systemEnergyCentreGroups = systemEnergyCentreResult.systemEnergyCentreGroups?.ConvertAll(x => new SystemEnergyCentreGroup(x));
+            }
+        }
+
+        public SystemEnergyCentreResult(string uniqueId, string name, string source, SystemEnergyCentreDataType systemEnergyCentreDataType, IEnumerable<SystemEnergyCentreGroup> systemEnergyCentreGroups)
+            : base(name, source, uniqueId)
+        {
+            SystemEnergyCentreDataType = systemEnergyCentreDataType;
+            this.systemEnergyCentreGroups = systemEnergyCentreGroups.ToList().ConvertAll(x => new SystemEnergyCentreGroup(x));
+        }
+
+        public List<SystemEnergyCentreGroup> SystemEnergyCentreGroups
+        {
+            get
+            {
+                return systemEnergyCentreGroups?.ConvertAll(x => new SystemEnergyCentreGroup(x));
+            }
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            if (jObject.ContainsKey("SystemEnergyCentreDataType"))
+            {
+                SystemEnergyCentreDataType = Core.Query.Enum<SystemEnergyCentreDataType>(jObject.Value<string>("SystemEnergyCentreDataType"));
+            }
+
+            if (jObject.ContainsKey("SystemEnergyCentreGroups"))
+            {
+                JArray jArray = jObject.Value<JArray>("SystemEnergyCentreGroups");
+                if(jArray != null)
+                {
+                    systemEnergyCentreGroups = new List<SystemEnergyCentreGroup>();
+                    foreach(JObject jObject_SystemEnergyCentreGroupResult in jArray)
+                    {
+                        systemEnergyCentreGroups.Add(new SystemEnergyCentreGroup(jObject_SystemEnergyCentreGroupResult));
+                    }
+
+                }
+            }
+
+            return result;
+        }
+
+        public override JObject ToJObject()
+        {
+            JObject jObject = base.ToJObject();
+            if (jObject == null)
+            {
+                return null;
+            }
+
+            jObject.Add("SystemEnergyCentreDataType", SystemEnergyCentreDataType.ToString());
+
+            if(systemEnergyCentreGroups != null)
+            {
+                JArray jArray = new JArray();
+                foreach(SystemEnergyCentreGroup systemEnergyCentreGroup in systemEnergyCentreGroups)
+                {
+                    jArray.Add(systemEnergyCentreGroup.ToJObject());
+                }
+
+                jObject.Add("SystemEnergyCentreGroups", jArray);
+            }
+
+            return jObject;
+        }
+    }
+}
