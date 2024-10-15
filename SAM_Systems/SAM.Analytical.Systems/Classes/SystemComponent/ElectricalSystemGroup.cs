672c90c17c440e1de1f3f4e2f588a3de635bdc3a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-10-15 15:02:23 +0200

    ElectricalSystemGroup Initial Implementation

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/ElectricalSystemGroup.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core.Systems;
+
+namespace SAM.Analytical.Systems
+{
+    public class ElectricalSystemGroup : SystemGroup<ElectricalSystem>
+    {
+        public ElectricalSystemGroup()
+            : base()
+        {
+        }
+
+        public ElectricalSystemGroup(string name)
+            : base(name)
+        {
+        }
+
+        public ElectricalSystemGroup(JObject jObject)
+            : base(jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public ElectricalSystemGroup(ElectricalSystemGroup electricalSystemGroup)
+            : base(electricalSystemGroup)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            return true;
+        }
+
+        public override JObject ToJObject()
+        {
+            JObject result = base.ToJObject();
+            if (result == null)
+            {
+                return null;
+            }
+
+            return result;
+        }
+    }
+}
