81c664a62da4b54c3a891a25aa91857ef4d0aead
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-10-28 18:02:20 +0100

    Code Reorganization

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/Result/SystemAirSourceDirectAbsorptionChillerResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+using SAM.Core.Systems;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Systems
+{
+    public class SystemAirSourceDirectAbsorptionChillerResult : SystemIndexedDoublesResult, ISystemComponentResult
+    {
+        public SystemAirSourceDirectAbsorptionChillerResult(string uniqueId, string name, string source, Dictionary<AirSourceDirectAbsorptionChillerDataType, IndexedDoubles> dictionary)
+            : base(uniqueId, name, source, Core.Systems.Query.Dictionary(dictionary))
+        {
+        }
+
+        public SystemAirSourceDirectAbsorptionChillerResult(JObject jObject)
+            : base(jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public SystemAirSourceDirectAbsorptionChillerResult(SystemAirSourceDirectAbsorptionChillerResult systemAirSourceDirectAbsorptionChillerResult)
+            : base(systemAirSourceDirectAbsorptionChillerResult)
+        {
+
+        }
+    }
+}
---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/Result/SystemIceStorageChillerResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+using SAM.Core.Systems;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Systems
+{
+    public class SystemIceStorageChillerResult : SystemIndexedDoublesResult, ISystemComponentResult
+    {
+        public SystemIceStorageChillerResult(string uniqueId, string name, string source, Dictionary<IceStorageChillerDataType, IndexedDoubles> dictionary)
+            : base(uniqueId, name, source, Core.Systems.Query.Dictionary(dictionary))
+        {
+        }
+
+        public SystemIceStorageChillerResult(JObject jObject)
+            : base(jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public SystemIceStorageChillerResult(SystemIceStorageChillerResult systemIceStorageChillerResult)
+            : base(systemIceStorageChillerResult)
+        {
+
+        }
+    }
+}
