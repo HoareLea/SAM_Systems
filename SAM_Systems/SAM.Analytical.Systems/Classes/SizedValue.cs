35a9b31d952165554628d38b17caf2ff8c9f82dd
Michal Dengusiak <tassimulation@gmail.com>
2024-12-04 11:28:20 +0000

    adjustment to Duty, DesignConditionNames from names

--- SAM_Systems/SAM.Analytical.Systems/Classes/SizedValue.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SizedValue.cs
@@ -5,15 +5,11 @@ namespace SAM.Analytical.Systems
 {
     public class SizedValue : SizableValue
     {
-        private HashSet<string> names;
-
         public double SizeFraction { get; set; }
 
-        public SizedValue(double value, double sizeFraction, IEnumerable<string> names)
+        public SizedValue(double value, double sizeFraction)
             : base(value)
         {
-            this.names = names == null ? null : new HashSet<string>(names);
-
             SizeFraction = sizeFraction;
         }
 
@@ -23,7 +19,6 @@ namespace SAM.Analytical.Systems
             if(sizedValue != null)
             {
                 SizeFraction = sizedValue.SizeFraction;
-                names = sizedValue.names == null ? null : new HashSet<string>(sizedValue.names);
             }
         }
 
@@ -31,14 +26,6 @@ namespace SAM.Analytical.Systems
             :base(jObject)
         {
 
-        }
-
-        public HashSet<string> Names
-        {
-            get
-            {
-                return names == null ? null : new HashSet<string>(names);
-            }
         }
 
         public override bool FromJObject(JObject jObject)
@@ -58,21 +45,6 @@ namespace SAM.Analytical.Systems
                 SizeFraction = jObject.Value<double>("SizeFraction");
             }
 
-            if (jObject.ContainsKey("Names"))
-            {
-                names = new HashSet<string>();
-
-                foreach(string name in jObject.Value<JArray>("Names"))
-                {
-                    if(name == null)
-                    {
-                        continue;
-                    }
-
-                    names.Add(name);
-                }               
-            }
-
             return true;
         }
 
@@ -87,17 +59,6 @@ namespace SAM.Analytical.Systems
             if(!double.IsNaN(SizeFraction))
             {
                 result.Add("SizeFraction", SizeFraction);
-            }
-
-            if (names != null)
-            {
-                JArray jArray = new JArray();
-                foreach (string name in names)
-                {
-                    jArray.Add(name);
-                }
-
-                result.Add("Names", jArray);
             }
 
             return result;
