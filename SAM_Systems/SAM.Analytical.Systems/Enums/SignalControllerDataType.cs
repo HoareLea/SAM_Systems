f4d6eca6cb421b4aaffeb5a0138ec2f4a0b14909
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-12-05 17:08:54 +0100

    SystemSignalControllerResult and SystemValueControllerResult Implemented

---
+++ SAM_Systems/SAM.Analytical.Systems/Enums/SignalControllerDataType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Analytical.Systems
+{
+    [Description("Signal Controller Data Type")]
+    public enum SignalControllerDataType
+    {
+        [Description("Signal")] Signal = 1,
+    }
+}
+
