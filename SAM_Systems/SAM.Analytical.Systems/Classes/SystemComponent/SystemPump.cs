33cbc2f403e2327ceb2eceb3299f1f70c8df9fd5
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-12-10 11:46:55 +0100

    SystemPump  Properties Updated

--- SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemPump.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemPump.cs
@@ -1,10 +1,23 @@
 ﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
 using SAM.Core.Systems;
 
 namespace SAM.Analytical.Systems
 {
     public class SystemPump : SystemComponent
     {
+        public ModifiableValue OverallEfficiency { get; set; }
+        
+        public double Pressure { get; set; }
+
+        public double DesignFrowRate { get; set; }
+
+        public double Capacity { get; set; }
+
+        public ModifiableValue PartLoad { get; set; }
+
+        public FanControlType FanControlType { get; set; }
+
         public SystemPump(string name)
             : base(name)
         {
@@ -29,8 +42,8 @@ namespace SAM.Analytical.Systems
             {
                 return Core.Systems.Create.SystemConnectorManager
                 (
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.In, 1),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.Out, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.In, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.Out, 1),
                     Core.Systems.Create.SystemConnector<IControlSystem>()
                 );
             }
@@ -38,12 +51,81 @@ namespace SAM.Analytical.Systems
 
         public override bool FromJObject(JObject jObject)
         {
-            return base.FromJObject(jObject);
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            if (jObject.ContainsKey("FanControlType"))
+            {
+                FanControlType = Core.Query.Enum<FanControlType>(jObject.Value<string>("FanControlType"));
+            }
+
+            if (jObject.ContainsKey("OverallEfficiency"))
+            {
+                OverallEfficiency = Core.Create.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("OverallEfficiency"));
+            }
+
+            if (jObject.ContainsKey("Pressure"))
+            {
+                Pressure = jObject.Value<double>("Pressure");
+            }
+
+            if (jObject.ContainsKey("DesignFrowRate"))
+            {
+                DesignFrowRate = jObject.Value<double>("DesignFrowRate");
+            }
+
+            if (jObject.ContainsKey("Capacity"))
+            {
+                Capacity = jObject.Value<double>("Capacity");
+            }
+
+            if (jObject.ContainsKey("PartLoad"))
+            {
+                PartLoad = Core.Create.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("PartLoad"));
+            }
+
+            return true;
         }
 
         public override JObject ToJObject()
         {
-            return base.ToJObject();
+            JObject result = base.ToJObject();
+            if (result == null)
+            {
+                return null;
+            }
+
+            result.Add("FanControlType", FanControlType.ToString());
+
+            if(OverallEfficiency != null)
+            {
+                result.Add("OverallEfficiency", OverallEfficiency.ToJObject());
+            }
+
+            if(!double.IsNaN(Pressure))
+            {
+                result.Add("Pressure", Pressure);
+            }
+
+            if (!double.IsNaN(DesignFrowRate))
+            {
+                result.Add("DesignFrowRate", DesignFrowRate);
+            }
+
+            if (!double.IsNaN(Capacity))
+            {
+                result.Add("Capacity", Capacity);
+            }
+
+            if (PartLoad != null)
+            {
+                result.Add("PartLoad", PartLoad.ToJObject());
+            }
+
+            return result;
         }
     }
 }
---
+++ SAM_Systems/SAM.Analytical.Systems/Enums/FanControlType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Analytical.Systems
+{
+    [Description("Fan Control Type")]
+    public enum FanControlType
+    {
+        [Description("Fixed Speed")] FixedSpeed,
+        [Description("Variable Speed")] VariableSpeed,
+    }
+}
+
