ea6f26897903a00f1fd2e74b4d361ecfa272c9e2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-12-20 13:42:21 +0100

    SystemWaterSourceChiller Properties Added

--- SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemWaterSourceChiller.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemWaterSourceChiller.cs
@@ -1,10 +1,20 @@
 ﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
 using SAM.Core.Systems;
 
 namespace SAM.Analytical.Systems
 {
     public class SystemWaterSourceChiller : SystemChiller
     {
+        public ModifiableValue Setpoint { get; set; }
+        public ModifiableValue Efficiency { get; set; }
+        public double Capacity1 { get; set; }
+        public double DesignPressureDrop1 { get; set; }
+        public double DesignTemperatureDifference1 { get; set; }
+        public double Capacity2 { get; set; }
+        public double DesignPressureDrop2 { get; set; }
+        public double DesignTemperatureDifference2 { get; set; }
+
         public SystemWaterSourceChiller(string name)
             : base(name)
         {
@@ -14,7 +24,17 @@ namespace SAM.Analytical.Systems
         public SystemWaterSourceChiller(SystemWaterSourceChiller waterSourceSystemChiller)
             : base(waterSourceSystemChiller)
         {
-
+            if(waterSourceSystemChiller != null)
+            {
+                Setpoint = waterSourceSystemChiller.Setpoint?.Clone();
+                Efficiency = waterSourceSystemChiller.Efficiency?.Clone();
+                Capacity1 = waterSourceSystemChiller.Capacity1;
+                DesignPressureDrop1 = waterSourceSystemChiller.DesignPressureDrop1;
+                DesignTemperatureDifference1 = waterSourceSystemChiller.DesignTemperatureDifference1;
+                Capacity2 = waterSourceSystemChiller.Capacity2;
+                DesignPressureDrop2 = waterSourceSystemChiller.DesignPressureDrop2;
+                DesignTemperatureDifference2 = waterSourceSystemChiller.DesignTemperatureDifference2;
+            }
         }
 
         public SystemWaterSourceChiller(JObject jObject)
@@ -29,10 +49,10 @@ namespace SAM.Analytical.Systems
             {
                 return Core.Systems.Create.SystemConnectorManager
                 (
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.In, 1),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.Out, 1),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.In, 2),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.Out, 2),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.In, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.Out, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.In, 2),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.Out, 2),
                     Core.Systems.Create.SystemConnector<IControlSystem>()
                 );
             }
@@ -40,12 +60,104 @@ namespace SAM.Analytical.Systems
 
         public override bool FromJObject(JObject jObject)
         {
-            return base.FromJObject(jObject);
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            if (jObject.ContainsKey("Setpoint"))
+            {
+                Setpoint = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Setpoint"));
+            }
+
+            if (jObject.ContainsKey("Efficiency"))
+            {
+                Efficiency = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Efficiency"));
+            }
+
+            if (jObject.ContainsKey("Capacity1"))
+            {
+                Capacity1 = jObject.Value<double>("Capacity1");
+            }
+
+            if (jObject.ContainsKey("DesignPressureDrop1"))
+            {
+                DesignPressureDrop1 = jObject.Value<double>("DesignPressureDrop1");
+            }
+
+            if (jObject.ContainsKey("DesignTemperatureDifference1"))
+            {
+                DesignTemperatureDifference1 = jObject.Value<double>("DesignTemperatureDifference1");
+            }
+
+            if (jObject.ContainsKey("Capacity2"))
+            {
+                Capacity2 = jObject.Value<double>("Capacity2");
+            }
+
+            if (jObject.ContainsKey("DesignPressureDrop2"))
+            {
+                DesignPressureDrop2 = jObject.Value<double>("DesignPressureDrop2");
+            }
+
+            if (jObject.ContainsKey("DesignTemperatureDifference2"))
+            {
+                DesignTemperatureDifference2 = jObject.Value<double>("DesignTemperatureDifference2");
+            }
+
+            return result;
         }
 
         public override JObject ToJObject()
         {
-            return base.ToJObject();
+            JObject result = base.ToJObject();
+            if(result == null)
+            {
+                return result;
+            }
+
+            if(Setpoint != null)
+            {
+                result.Add("Setpoint", Setpoint.ToJObject());
+            }
+
+            if (Efficiency != null)
+            {
+                result.Add("Efficiency", Efficiency.ToJObject());
+            }
+
+            if (!double.IsNaN(Capacity1))
+            {
+                result.Add("Capacity1", Capacity1);
+            }
+
+            if (!double.IsNaN(DesignPressureDrop1))
+            {
+                result.Add("DesignPressureDrop1", DesignPressureDrop1);
+            }
+
+            if (!double.IsNaN(DesignTemperatureDifference1))
+            {
+                result.Add("DesignTemperatureDifference1", DesignTemperatureDifference1);
+            }
+
+            if (!double.IsNaN(Capacity2))
+            {
+                result.Add("Capacity2", Capacity2);
+            }
+
+            if (!double.IsNaN(DesignPressureDrop2))
+            {
+                result.Add("DesignPressureDrop2", DesignPressureDrop2);
+            }
+
+            if (!double.IsNaN(DesignTemperatureDifference2))
+            {
+                result.Add("DesignTemperatureDifference2", DesignTemperatureDifference2);
+            }
+
+            return result;
         }
     }
 }
