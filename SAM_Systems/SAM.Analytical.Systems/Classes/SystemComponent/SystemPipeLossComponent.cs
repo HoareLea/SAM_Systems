2e7da96a2857636247287e54c1525fbed3959782
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-12-20 12:03:36 +0100

    SystemPipeLossComponent Properties Added

--- SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemPipeLossComponent.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemPipeLossComponent.cs
@@ -1,14 +1,36 @@
 ﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
 using SAM.Core.Systems;
 
 namespace SAM.Analytical.Systems
 {
     public class SystemPipeLossComponent : SystemComponent
     {
+        public double DesignPressureDrop { get; set; }
+        public double Capacity { get; set; }
+        public double Length { get; set; }
+        public double InsidePipeDiameter { get; set; }
+        public double OutsidePipeDiameter { get; set; }
+        public double PipeConductivity { get; set; }
+        public double InsulationThickness { get; set; }
+        public double InsulationConductivity { get; set; }
+        public ModifiableValue AmbientTemperature { get; set; }
+
         public SystemPipeLossComponent(SystemPipeLossComponent systemPipeLossComponent)
             : base(systemPipeLossComponent)
         {
-
+            if (systemPipeLossComponent != null)
+            {
+                DesignPressureDrop = systemPipeLossComponent.DesignPressureDrop;
+                Capacity = systemPipeLossComponent.Capacity;
+                Length = systemPipeLossComponent.Length;
+                InsidePipeDiameter = systemPipeLossComponent.InsidePipeDiameter;
+                OutsidePipeDiameter = systemPipeLossComponent.OutsidePipeDiameter;
+                PipeConductivity = systemPipeLossComponent.PipeConductivity;
+                InsulationThickness = systemPipeLossComponent.InsulationThickness;
+                InsulationConductivity = systemPipeLossComponent.InsulationConductivity;
+                AmbientTemperature = systemPipeLossComponent.AmbientTemperature?.Clone();
+            }
         }
 
         public SystemPipeLossComponent(string name)
@@ -37,12 +59,114 @@ namespace SAM.Analytical.Systems
 
         public override bool FromJObject(JObject jObject)
         {
-            return base.FromJObject(jObject);
+            bool result = base.FromJObject(jObject);
+            if(!result)
+            {
+                return result;
+            }
+
+            if(jObject.ContainsKey("DesignPressureDrop"))
+            {
+                DesignPressureDrop = jObject.Value<double>("DesignPressureDrop");
+            }
+
+            if (jObject.ContainsKey("Capacity"))
+            {
+                Capacity = jObject.Value<double>("Capacity");
+            }
+
+            if (jObject.ContainsKey("Length"))
+            {
+                Length = jObject.Value<double>("Length");
+            }
+
+            if (jObject.ContainsKey("InsidePipeDiameter"))
+            {
+                InsidePipeDiameter = jObject.Value<double>("InsidePipeDiameter");
+            }
+
+            if (jObject.ContainsKey("OutsidePipeDiameter"))
+            {
+                OutsidePipeDiameter = jObject.Value<double>("OutsidePipeDiameter");
+            }
+
+            if (jObject.ContainsKey("PipeConductivity"))
+            {
+                PipeConductivity = jObject.Value<double>("PipeConductivity");
+            }
+
+            if (jObject.ContainsKey("InsulationThickness"))
+            {
+                InsulationThickness = jObject.Value<double>("InsulationThickness");
+            }
+
+            if (jObject.ContainsKey("InsulationConductivity"))
+            {
+                InsulationConductivity = jObject.Value<double>("InsulationConductivity");
+            }
+
+            if (jObject.ContainsKey("AmbientTemperature"))
+            {
+                AmbientTemperature = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("AmbientTemperature"));
+            }
+
+            return result;
         }
 
         public override JObject ToJObject()
         {
-            return base.ToJObject();
+            JObject result = base.ToJObject();
+            if(result == null)
+            {
+                return result;
+            }
+
+            if(!double.IsNaN(DesignPressureDrop))
+            {
+                result.Add("DesignPressureDrop", DesignPressureDrop);
+            }
+
+            if (!double.IsNaN(Capacity))
+            {
+                result.Add("Capacity", Capacity);
+            }
+
+            if (!double.IsNaN(Length))
+            {
+                result.Add("Length", Length);
+            }
+
+            if (!double.IsNaN(InsidePipeDiameter))
+            {
+                result.Add("InsidePipeDiameter", InsidePipeDiameter);
+            }
+
+            if (!double.IsNaN(OutsidePipeDiameter))
+            {
+                result.Add("OutsidePipeDiameter", OutsidePipeDiameter);
+            }
+
+            if (!double.IsNaN(PipeConductivity))
+            {
+                result.Add("PipeConductivity", PipeConductivity);
+            }
+
+            if (!double.IsNaN(InsulationThickness))
+            {
+                result.Add("InsulationThickness", InsulationThickness);
+            }
+
+            if (!double.IsNaN(InsulationConductivity))
+            {
+                result.Add("InsulationConductivity", InsulationConductivity);
+            }
+
+            if(AmbientTemperature != null)
+            {
+                result.Add("AmbientTemperature", AmbientTemperature.ToJObject());
+            }
+
+            return result;
         }
     }
 }
