8b08a4f22a80bf2e9f02c73c29794f2f4dc7ab2c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-08 13:11:50 +0100

    SystemWaterSourceAbsorptionChiller Properties Added

--- SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemWaterSourceAbsorptionChiller.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemWaterSourceAbsorptionChiller.cs
@@ -1,13 +1,22 @@
 ﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
 using SAM.Core.Systems;
 
 namespace SAM.Analytical.Systems
 {
     public class SystemWaterSourceAbsorptionChiller : SystemChiller
     {
+        public ModifiableValue Setpoint { get; set; }
+        public ModifiableValue Efficiency { get; set; }
+        public double Capacity1 { get; set; }
         public double DesignPressureDrop1 { get; set; }
+        public double Capacity2 { get; set; }
         public double DesignPressureDrop2 { get; set; }
+        public double Capacity3 { get; set; }
         public double DesignPressureDrop3 { get; set; }
+        public ModifiableValue AncillaryLoad { get; set; }
+        public ModifiableValue MinimalOutSourceTemperature { get; set; }
+        public bool LossesInSizing { get; set; }
 
         public SystemWaterSourceAbsorptionChiller(string name)
             : base(name)
@@ -20,9 +29,17 @@ namespace SAM.Analytical.Systems
         {
             if (waterSourceAbsorptionSystemChiller != null)
             {
+                Setpoint = waterSourceAbsorptionSystemChiller.Setpoint?.Clone();
+                Efficiency = waterSourceAbsorptionSystemChiller.Efficiency?.Clone();
+                Capacity1 = waterSourceAbsorptionSystemChiller.Capacity1;
+                Capacity2 = waterSourceAbsorptionSystemChiller.Capacity2;
+                Capacity3 = waterSourceAbsorptionSystemChiller.Capacity3;
                 DesignPressureDrop1 = waterSourceAbsorptionSystemChiller.DesignPressureDrop1;
                 DesignPressureDrop2 = waterSourceAbsorptionSystemChiller.DesignPressureDrop2;
                 DesignPressureDrop3 = waterSourceAbsorptionSystemChiller.DesignPressureDrop3;
+                AncillaryLoad = waterSourceAbsorptionSystemChiller.AncillaryLoad?.Clone();
+                MinimalOutSourceTemperature = waterSourceAbsorptionSystemChiller.MinimalOutSourceTemperature?.Clone();
+                LossesInSizing = waterSourceAbsorptionSystemChiller.LossesInSizing;
             }
         }
 
@@ -38,12 +55,12 @@ namespace SAM.Analytical.Systems
             {
                 return Core.Systems.Create.SystemConnectorManager
                 (
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.In, 1),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.Out, 1),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.In, 2),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.Out, 2),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.In, 3),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.Out, 3),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.In, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.Out, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.In, 2),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.Out, 2),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.In, 3),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.Out, 3),
                     Core.Systems.Create.SystemConnector<IControlSystem>()
                 );
             }
@@ -55,6 +72,31 @@ namespace SAM.Analytical.Systems
             if (!result)
             {
                 return result;
+            }
+
+            if (jObject.ContainsKey("Setpoint"))
+            {
+                Setpoint = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Setpoint"));
+            }
+
+            if (jObject.ContainsKey("Efficiency"))
+            {
+                Efficiency = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Efficiency"));
+            }
+
+            if (jObject.ContainsKey("Capacity1"))
+            {
+                Capacity1 = jObject.Value<double>("Capacity1");
+            }
+
+            if (jObject.ContainsKey("Capacity2"))
+            {
+                Capacity2 = jObject.Value<double>("Capacity2");
+            }
+
+            if (jObject.ContainsKey("Capacity3"))
+            {
+                Capacity3 = jObject.Value<double>("Capacity3");
             }
 
             if (jObject.ContainsKey("DesignPressureDrop1"))
@@ -72,6 +114,21 @@ namespace SAM.Analytical.Systems
                 DesignPressureDrop3 = jObject.Value<double>("DesignPressureDrop3");
             }
 
+            if (jObject.ContainsKey("AncillaryLoad"))
+            {
+                AncillaryLoad = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("AncillaryLoad"));
+            }
+
+            if (jObject.ContainsKey("LossesInSizing"))
+            {
+                LossesInSizing = jObject.Value<bool>("LossesInSizing");
+            }
+
+            if (jObject.ContainsKey("MinimalOutSourceTemperature"))
+            {
+                MinimalOutSourceTemperature = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("MinimalOutSourceTemperature"));
+            }
+
             return result;
         }
 
@@ -81,6 +138,31 @@ namespace SAM.Analytical.Systems
             if (result == null)
             {
                 return result;
+            }
+
+            if (Setpoint != null)
+            {
+                result.Add("Setpoint", Setpoint.ToJObject());
+            }
+
+            if (Efficiency != null)
+            {
+                result.Add("Efficiency", Efficiency.ToJObject());
+            }
+
+            if (!double.IsNaN(Capacity1))
+            {
+                result.Add("Capacity1", Capacity1);
+            }
+
+            if (!double.IsNaN(Capacity2))
+            {
+                result.Add("Capacity2", Capacity2);
+            }
+
+            if (!double.IsNaN(Capacity3))
+            {
+                result.Add("Capacity3", Capacity3);
             }
 
             if (!double.IsNaN(DesignPressureDrop1))
@@ -97,6 +179,18 @@ namespace SAM.Analytical.Systems
             {
                 result.Add("DesignPressureDrop3", DesignPressureDrop3);
             }
+
+            if (AncillaryLoad != null)
+            {
+                result.Add("AncillaryLoad", AncillaryLoad.ToJObject());
+            }
+
+            if (MinimalOutSourceTemperature != null)
+            {
+                result.Add("MinimalOutSourceTemperature", MinimalOutSourceTemperature.ToJObject());
+            }
+
+            result.Add("LossesInSizing", LossesInSizing);
 
             return result;
         }
