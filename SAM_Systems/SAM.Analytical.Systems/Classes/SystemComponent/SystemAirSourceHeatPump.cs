142813a087ed95b469a44a01b66b478041fe8aa6
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-09 11:56:55 +0100

    SystemAirSourceHeatPump Properties Added

--- SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemAirSourceHeatPump.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemAirSourceHeatPump.cs
@@ -1,10 +1,31 @@
 ﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
 using SAM.Core.Systems;
 
 namespace SAM.Analytical.Systems
 {
     public class SystemAirSourceHeatPump : SystemHeatPump
     {
+        public HeatPumpType HeatPumpType { get; set; }
+        public SizableValue CoolingCapacity { get; set; }
+        public ModifiableValue CoolingPower { get; set; }
+        public ModifiableValue HeatingCapacity { get; set; }
+        public ModifiableValue HeatingPower { get; set; }
+        public ModifiableValue CondenserFanLoad { get; set; }
+        public double HeatingCoolingDutyRatio { get; set; }
+        public double HeatingCapacityPowerRatio { get; set; }
+        public double CoolingCapacityPowerRatio { get; set; }
+        public double MaxDemandFanRatio { get; set; }
+        public double StandbyPower { get; set; }
+        public double ADFHeatingMode { get; set; }
+        public double ADFCoolingMode { get; set; }
+        public double PortHeatingPower { get; set; }
+        public double PortCoolingPower { get; set; }
+        public double WaterPipeLength { get; set; }
+        public ModifiableValue AncillaryLoad { get; set; }
+        public double HeatSizeFraction { get; set; }
+
+
         public SystemAirSourceHeatPump(string name)
             : base(name)
         {
@@ -14,7 +35,27 @@ namespace SAM.Analytical.Systems
         public SystemAirSourceHeatPump(SystemAirSourceHeatPump systemAirSourceHeatPump)
             : base(systemAirSourceHeatPump)
         {
-
+            if(systemAirSourceHeatPump != null)
+            {
+                HeatPumpType = systemAirSourceHeatPump.HeatPumpType;
+                CoolingCapacity = systemAirSourceHeatPump.CoolingCapacity?.Clone();
+                CoolingPower = systemAirSourceHeatPump?.CoolingPower?.Clone();
+                HeatingCapacity = systemAirSourceHeatPump.HeatingCapacity?.Clone();
+                HeatingPower = systemAirSourceHeatPump.HeatingPower?.Clone();
+                CondenserFanLoad = systemAirSourceHeatPump.CondenserFanLoad?.Clone();
+                HeatingCoolingDutyRatio = systemAirSourceHeatPump.HeatingCoolingDutyRatio;
+                HeatingCapacityPowerRatio = systemAirSourceHeatPump.HeatingCapacityPowerRatio;
+                CoolingCapacityPowerRatio = systemAirSourceHeatPump.CoolingCapacityPowerRatio;
+                MaxDemandFanRatio = systemAirSourceHeatPump.MaxDemandFanRatio;
+                StandbyPower = systemAirSourceHeatPump.StandbyPower;
+                ADFHeatingMode = systemAirSourceHeatPump.ADFHeatingMode;
+                ADFCoolingMode = systemAirSourceHeatPump.ADFCoolingMode;
+                PortHeatingPower = systemAirSourceHeatPump.PortHeatingPower;
+                PortCoolingPower = systemAirSourceHeatPump.PortCoolingPower;
+                WaterPipeLength = systemAirSourceHeatPump.WaterPipeLength;
+                AncillaryLoad = systemAirSourceHeatPump.AncillaryLoad?.Clone();
+                HeatSizeFraction = systemAirSourceHeatPump.HeatSizeFraction;
+            }
         }
 
         public SystemAirSourceHeatPump(JObject jObject)
@@ -29,8 +70,8 @@ namespace SAM.Analytical.Systems
             {
                 return Core.Systems.Create.SystemConnectorManager
                 (
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.In, 1),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.Out, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.In, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.Out, 1),
                     Core.Systems.Create.SystemConnector<IControlSystem>()
                 );
             }
@@ -38,12 +79,201 @@ namespace SAM.Analytical.Systems
 
         public override bool FromJObject(JObject jObject)
         {
-            return base.FromJObject(jObject);
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            if (jObject.ContainsKey("HeatPumpType"))
+            {
+                HeatPumpType = Core.Query.Enum<HeatPumpType>(jObject.Value<string>("HeatPumpType"));
+            }
+
+            if (jObject.ContainsKey("CoolingCapacity"))
+            {
+                CoolingCapacity = Core.Query.IJSAMObject<SizableValue>(jObject.Value<JObject>("CoolingCapacity"));
+            }
+
+            if (jObject.ContainsKey("CoolingPower"))
+            {
+                CoolingPower = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("CoolingPower"));
+            }
+
+            if (jObject.ContainsKey("HeatingCapacity"))
+            {
+                HeatingCapacity = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("HeatingCapacity"));
+            }
+
+            if (jObject.ContainsKey("HeatingPower"))
+            {
+                HeatingPower = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("HeatingPower"));
+            }
+
+            if (jObject.ContainsKey("CondenserFanLoad"))
+            {
+                CondenserFanLoad = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("CondenserFanLoad"));
+            }
+
+            if (jObject.ContainsKey("HeatingCoolingDutyRatio"))
+            {
+                HeatingCoolingDutyRatio = jObject.Value<double>("HeatingCoolingDutyRatio");
+            }
+
+            if (jObject.ContainsKey("HeatingCapacityPowerRatio"))
+            {
+                HeatingCapacityPowerRatio = jObject.Value<double>("HeatingCapacityPowerRatio");
+            }
+
+            if (jObject.ContainsKey("CoolingCapacityPowerRatio"))
+            {
+                CoolingCapacityPowerRatio = jObject.Value<double>("CoolingCapacityPowerRatio");
+            }
+
+            if (jObject.ContainsKey("MaxDemandFanRatio"))
+            {
+                MaxDemandFanRatio = jObject.Value<double>("MaxDemandFanRatio");
+            }
+
+            if (jObject.ContainsKey("StandbyPower"))
+            {
+                StandbyPower = jObject.Value<double>("StandbyPower");
+            }
+
+            if (jObject.ContainsKey("ADFHeatingMode"))
+            {
+                ADFHeatingMode = jObject.Value<double>("ADFHeatingMode");
+            }
+
+            if (jObject.ContainsKey("ADFCoolingMode"))
+            {
+                ADFCoolingMode = jObject.Value<double>("ADFCoolingMode");
+            }
+
+            if (jObject.ContainsKey("PortHeatingPower"))
+            {
+                PortHeatingPower = jObject.Value<double>("PortHeatingPower");
+            }
+
+            if (jObject.ContainsKey("PortCoolingPower"))
+            {
+                PortCoolingPower = jObject.Value<double>("PortCoolingPower");
+            }
+
+            if (jObject.ContainsKey("WaterPipeLength"))
+            {
+                WaterPipeLength = jObject.Value<double>("WaterPipeLength");
+            }
+
+            if (jObject.ContainsKey("AncillaryLoad"))
+            {
+                AncillaryLoad = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("AncillaryLoad"));
+            }
+
+            if (jObject.ContainsKey("HeatSizeFraction"))
+            {
+                HeatSizeFraction = jObject.Value<double>("HeatSizeFraction");
+            }
+
+            return result;
         }
 
         public override JObject ToJObject()
         {
-            return base.ToJObject();
+            JObject result = base.ToJObject();
+            if (result == null)
+            {
+                return result;
+            }
+
+            result.Add("HeatPumpType", HeatPumpType.ToString());
+
+            if (CoolingCapacity != null)
+            {
+                result.Add("CoolingCapacity", CoolingCapacity.ToJObject());
+            }
+
+            if (CoolingPower != null)
+            {
+                result.Add("CoolingPower", CoolingPower.ToJObject());
+            }
+
+            if (HeatingCapacity != null)
+            {
+                result.Add("HeatingCapacity", HeatingCapacity.ToJObject());
+            }
+
+            if (HeatingPower != null)
+            {
+                result.Add("HeatingPower", HeatingPower.ToJObject());
+            }
+
+            if (CondenserFanLoad != null)
+            {
+                result.Add("CondenserFanLoad", CondenserFanLoad.ToJObject());
+            }
+
+            if (!double.IsNaN(HeatingCoolingDutyRatio))
+            {
+                result.Add("HeatingCoolingDutyRatio", HeatingCoolingDutyRatio);
+            }
+
+            if (!double.IsNaN(HeatingCapacityPowerRatio))
+            {
+                result.Add("HeatingCapacityPowerRatio", HeatingCapacityPowerRatio);
+            }
+
+            if (!double.IsNaN(CoolingCapacityPowerRatio))
+            {
+                result.Add("CoolingCapacityPowerRatio", CoolingCapacityPowerRatio);
+            }
+
+            if (!double.IsNaN(MaxDemandFanRatio))
+            {
+                result.Add("MaxDemandFanRatio", MaxDemandFanRatio);
+            }
+
+            if (!double.IsNaN(StandbyPower))
+            {
+                result.Add("StandbyPower", StandbyPower);
+            }
+
+            if (!double.IsNaN(ADFHeatingMode))
+            {
+                result.Add("ADFHeatingMode", ADFHeatingMode);
+            }
+
+            if (!double.IsNaN(ADFCoolingMode))
+            {
+                result.Add("ADFCoolingMode", ADFCoolingMode);
+            }
+
+            if (!double.IsNaN(PortHeatingPower))
+            {
+                result.Add("PortHeatingPower", PortHeatingPower);
+            }
+
+            if (!double.IsNaN(PortCoolingPower))
+            {
+                result.Add("PortCoolingPower", PortCoolingPower);
+            }
+
+            if (!double.IsNaN(WaterPipeLength))
+            {
+                result.Add("WaterPipeLength", WaterPipeLength);
+            }
+
+            if (AncillaryLoad != null)
+            {
+                result.Add("AncillaryLoad", AncillaryLoad.ToJObject());
+            }
+
+            if (!double.IsNaN(HeatSizeFraction))
+            {
+                result.Add("HeatSizeFraction", HeatSizeFraction);
+            }
+
+            return result;
         }
     }
 }
---
+++ SAM_Systems/SAM.Analytical.Systems/Enums/HeatPumpType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Analytical.Systems
+{
+    [Description("Heat Pump Type")]
+    public enum HeatPumpType
+    {
+        [Description("Single Split")] SingleSplit,
+        [Description("Multi Split")] MultiSplit,
+        [Description("VRF")] VRF,
+        [Description("VRFBC")] VRFBC,
+        [Description("HVRFBC")] HVRFBC,
+    }
+}
