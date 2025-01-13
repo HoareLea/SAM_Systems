4052fd6ca72a6bb1a770a319742e4dba084066ae
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-13 17:49:56 +0100

    SystemWindTurbine Properties Added

--- SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemWindTurbine.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemWindTurbine.cs
@@ -1,10 +1,19 @@
 ﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
 using SAM.Core.Systems;
 
 namespace SAM.Analytical.Systems
 {
     public class SystemWindTurbine : SystemComponent
     {
+        public double HubHeight { get; set; }
+        public double Area { get; set; }
+        public double MinSpeed { get; set; }
+        public double CutOffSpeed { get; set; }
+        public int Multiplicity { get; set; }
+        public ModifiableValue Efficiency { get; set; }
+
+
         public SystemWindTurbine(string name)
             : base(name)
         {
@@ -14,7 +23,15 @@ namespace SAM.Analytical.Systems
         public SystemWindTurbine(SystemWindTurbine systemWindTurbine)
             : base(systemWindTurbine)
         {
-
+            if(systemWindTurbine != null)
+            {
+                HubHeight = systemWindTurbine.HubHeight;
+                Area = systemWindTurbine.Area;
+                MinSpeed = systemWindTurbine.MinSpeed;
+                CutOffSpeed = systemWindTurbine.CutOffSpeed;
+                Multiplicity = systemWindTurbine.Multiplicity;
+                Efficiency = systemWindTurbine.Efficiency?.Clone();
+            }
         }
 
         public SystemWindTurbine(JObject jObject)
@@ -38,12 +55,81 @@ namespace SAM.Analytical.Systems
 
         public override bool FromJObject(JObject jObject)
         {
-            return base.FromJObject(jObject);
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            if (jObject.ContainsKey("HubHeight"))
+            {
+                HubHeight = jObject.Value<double>("HubHeight");
+            }
+
+            if (jObject.ContainsKey("Area"))
+            {
+                Area = jObject.Value<double>("Area");
+            }
+
+            if (jObject.ContainsKey("MinSpeed"))
+            {
+                MinSpeed = jObject.Value<double>("MinSpeed");
+            }
+
+            if (jObject.ContainsKey("CutOffSpeed"))
+            {
+                CutOffSpeed = jObject.Value<double>("CutOffSpeed");
+            }
+
+            if (jObject.ContainsKey("Multiplicity"))
+            {
+                Multiplicity = jObject.Value<int>("Multiplicity");
+            }
+
+            if (jObject.ContainsKey("Efficiency"))
+            {
+                Efficiency = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Efficiency"));
+            }
+
+            return result;
         }
 
         public override JObject ToJObject()
         {
-            return base.ToJObject();
+            JObject result = base.ToJObject();
+            if (result == null)
+            {
+                return result;
+            }
+
+            if (!double.IsNaN(HubHeight))
+            {
+                result.Add("HubHeight", HubHeight);
+            }
+
+            if (!double.IsNaN(Area))
+            {
+                result.Add("Area", Area);
+            }
+
+            if (!double.IsNaN(MinSpeed))
+            {
+                result.Add("MinSpeed", MinSpeed);
+            }
+
+            if (!double.IsNaN(CutOffSpeed))
+            {
+                result.Add("CutOffSpeed", CutOffSpeed);
+            }
+
+            result.Add("Multiplicity", Multiplicity);
+            
+            if (Efficiency != null)
+            {
+                result.Add("Efficiency", Efficiency.ToJObject());
+            }
+
+            return result;
         }
     }
 }
