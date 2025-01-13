10627df1d23ddd5c3643687a3f0dca1e42c6951b
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-13 17:40:52 +0100

    SystemPhotovoltaicPanel Properties Added

--- SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemPhotovoltaicPanel.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemPhotovoltaicPanel.cs
@@ -1,10 +1,27 @@
 ﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
 using SAM.Core.Systems;
 
 namespace SAM.Analytical.Systems
 {
     public class SystemPhotovoltaicPanel : SystemComponent
     {
+        public ModifiableValue PanelEfficiency { get; set; }
+        public SizableValue InverterSize { get; set; }
+        public int Multiplicity { get; set; }
+        public ModifiableValue InverterEfficiency { get; set; }
+        public bool UseZoneSurface { get; set; }
+        public double Area { get; set; }
+        public ModifiableValue Inclination { get; set; }
+        public ModifiableValue Orientation { get; set; }
+        public double Reflectance { get; set; }
+        public double MinIrradiance { get; set; }
+        public double NOCT { get; set; }
+        public double PowerTemperatureCoefficient { get; set; }
+        public bool UseSTC { get; set; }
+        public double OutputAtSTC { get; set; }
+        public double DeratingFactor { get; set; }
+
         public SystemPhotovoltaicPanel(string name)
             : base(name)
         {
@@ -14,7 +31,24 @@ namespace SAM.Analytical.Systems
         public SystemPhotovoltaicPanel(SystemPhotovoltaicPanel systemPhotovoltaicPanel)
             : base(systemPhotovoltaicPanel)
         {
-
+            if(systemPhotovoltaicPanel != null)
+            {
+                PanelEfficiency = systemPhotovoltaicPanel.PanelEfficiency?.Clone();
+                InverterEfficiency = systemPhotovoltaicPanel.InverterEfficiency?.Clone();
+                Multiplicity = systemPhotovoltaicPanel.Multiplicity;
+                InverterEfficiency = systemPhotovoltaicPanel?.InverterEfficiency?.Clone();
+                UseZoneSurface = systemPhotovoltaicPanel.UseZoneSurface;
+                Area = systemPhotovoltaicPanel.Area;
+                Inclination = systemPhotovoltaicPanel.Inclination?.Clone();
+                Orientation = systemPhotovoltaicPanel.Orientation?.Clone();
+                Reflectance = systemPhotovoltaicPanel.Reflectance;
+                MinIrradiance = systemPhotovoltaicPanel.MinIrradiance;
+                NOCT = systemPhotovoltaicPanel.NOCT;
+                PowerTemperatureCoefficient = systemPhotovoltaicPanel.PowerTemperatureCoefficient;
+                UseSTC = systemPhotovoltaicPanel.UseSTC;
+                OutputAtSTC = systemPhotovoltaicPanel.OutputAtSTC;
+                DeratingFactor = systemPhotovoltaicPanel.DeratingFactor;
+            }
         }
 
         public SystemPhotovoltaicPanel(JObject jObject)
@@ -38,12 +72,165 @@ namespace SAM.Analytical.Systems
 
         public override bool FromJObject(JObject jObject)
         {
-            return base.FromJObject(jObject);
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            if (jObject.ContainsKey("PanelEfficiency"))
+            {
+                PanelEfficiency = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("PanelEfficiency"));
+            }
+
+            if (jObject.ContainsKey("InverterSize"))
+            {
+                InverterSize = Core.Query.IJSAMObject<SizableValue>(jObject.Value<JObject>("InverterSize"));
+            }
+
+            if (jObject.ContainsKey("Multiplicity"))
+            {
+                Multiplicity = jObject.Value<int>("Multiplicity");
+            }
+
+            if (jObject.ContainsKey("InverterEfficiency"))
+            {
+                InverterEfficiency = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("InverterEfficiency"));
+            }
+
+            if (jObject.ContainsKey("UseZoneSurface"))
+            {
+                UseZoneSurface = jObject.Value<bool>("UseZoneSurface");
+            }
+
+            if (jObject.ContainsKey("Area"))
+            {
+                Area = jObject.Value<double>("Area");
+            }
+
+            if (jObject.ContainsKey("Inclination"))
+            {
+                Inclination = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Inclination"));
+            }
+
+            if (jObject.ContainsKey("Orientation"))
+            {
+                Orientation = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Orientation"));
+            }
+
+            if (jObject.ContainsKey("Reflectance"))
+            {
+                Reflectance = jObject.Value<double>("Reflectance");
+            }
+
+            if (jObject.ContainsKey("MinIrradiance"))
+            {
+                MinIrradiance = jObject.Value<double>("MinIrradiance");
+            }
+
+            if (jObject.ContainsKey("NOCT"))
+            {
+                NOCT = jObject.Value<double>("NOCT");
+            }
+
+            if (jObject.ContainsKey("PowerTemperatureCoefficient"))
+            {
+                PowerTemperatureCoefficient = jObject.Value<double>("PowerTemperatureCoefficient");
+            }
+
+            if (jObject.ContainsKey("UseSTC"))
+            {
+                UseSTC = jObject.Value<bool>("UseSTC");
+            }
+
+            if (jObject.ContainsKey("OutputAtSTC"))
+            {
+                OutputAtSTC = jObject.Value<double>("OutputAtSTC");
+            }
+
+            if (jObject.ContainsKey("DeratingFactor"))
+            {
+                DeratingFactor = jObject.Value<double>("DeratingFactor");
+            }
+
+            return result;
         }
 
         public override JObject ToJObject()
         {
-            return base.ToJObject();
+            JObject result = base.ToJObject();
+            if (result == null)
+            {
+                return result;
+            }
+
+            if (PanelEfficiency != null)
+            {
+                result.Add("PanelEfficiency", PanelEfficiency.ToJObject());
+            }
+
+            if (InverterSize != null)
+            {
+                result.Add("InverterSize", InverterSize.ToJObject());
+            }
+
+            result.Add("Multiplicity", Multiplicity);
+
+            if (InverterEfficiency != null)
+            {
+                result.Add("InverterEfficiency", InverterEfficiency.ToJObject());
+            }
+
+            result.Add("UseZoneSurface", UseZoneSurface);
+
+            if (!double.IsNaN(Area))
+            {
+                result.Add("Area", Area);
+            }
+
+            if (Inclination != null)
+            {
+                result.Add("Inclination", Inclination.ToJObject());
+            }
+
+            if (Orientation != null)
+            {
+                result.Add("Orientation", Orientation.ToJObject());
+            }
+
+            if (!double.IsNaN(Reflectance))
+            {
+                result.Add("Reflectance", Reflectance);
+            }
+
+            if (!double.IsNaN(MinIrradiance))
+            {
+                result.Add("MinIrradiance", MinIrradiance);
+            }
+
+            if (!double.IsNaN(NOCT))
+            {
+                result.Add("NOCT", NOCT);
+            }
+
+            if (!double.IsNaN(PowerTemperatureCoefficient))
+            {
+                result.Add("PowerTemperatureCoefficient", PowerTemperatureCoefficient);
+            }
+
+            result.Add("UseSTC", UseSTC);
+
+            if (!double.IsNaN(OutputAtSTC))
+            {
+                result.Add("OutputAtSTC", OutputAtSTC);
+            }
+
+            if (!double.IsNaN(DeratingFactor))
+            {
+                result.Add("DeratingFactor", DeratingFactor);
+            }
+
+            return result;
         }
     }
 }
