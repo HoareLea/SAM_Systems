55780c1ad3faff1a0c0f14103a81c950af59330f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-23 15:01:09 +0100

    SystemHeatingCoil properties updated

--- SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemHeatingCoil.cs
+++ SAM_Systems/SAM.Analytical.Systems/Classes/SystemComponent/SystemHeatingCoil.cs
@@ -1,11 +1,15 @@
 ﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
 using SAM.Core.Systems;
 
 namespace SAM.Analytical.Systems
 {
     public class SystemHeatingCoil : SystemComponent
     {
+        public ModifiableValue Setpoint { get; set; }
+        public ModifiableValue Efficiency { get; set; }
         public SizableValue Duty { get; set; }
+        public ModifiableValue MaximumOffcoil { get; set; }
 
         public SystemHeatingCoil(string name)
             : base(name)
@@ -18,7 +22,10 @@ namespace SAM.Analytical.Systems
         {
             if(systemHeatingCoil != null)
             {
-                Duty = systemHeatingCoil.Duty;
+                Setpoint = systemHeatingCoil.Setpoint?.Clone();
+                Efficiency = systemHeatingCoil.Efficiency?.Clone();
+                Duty = systemHeatingCoil.Duty?.Clone();
+                MaximumOffcoil = systemHeatingCoil.MaximumOffcoil?.Clone();
             }
         }
 
@@ -34,12 +41,12 @@ namespace SAM.Analytical.Systems
             {
                 return Core.Systems.Create.SystemConnectorManager
                 (
-                    Core.Systems.Create.SystemConnector<AirSystem>(Core.Direction.In, 2),
-                    Core.Systems.Create.SystemConnector<AirSystem>(Core.Direction.Out, 2),
+                    Core.Systems.Create.SystemConnector<AirSystem>(Direction.In, 2),
+                    Core.Systems.Create.SystemConnector<AirSystem>(Direction.Out, 2),
                     Core.Systems.Create.SystemConnector<IControlSystem>(),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.In, 1),
-                    Core.Systems.Create.SystemConnector<LiquidSystem>(Core.Direction.Out, 1),
-                    Core.Systems.Create.SystemConnector<ElectricalSystem>(Core.Direction.In)
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.In, 1),
+                    Core.Systems.Create.SystemConnector<LiquidSystem>(Direction.Out, 1),
+                    Core.Systems.Create.SystemConnector<ElectricalSystem>(Direction.In)
                 );
             }
         }
@@ -52,9 +59,24 @@ namespace SAM.Analytical.Systems
                 return result;
             }
 
+            if (jObject.ContainsKey("Setpoint"))
+            {
+                Setpoint = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Setpoint"));
+            }
+
+            if (jObject.ContainsKey("Efficiency"))
+            {
+                Efficiency = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("Efficiency"));
+            }
+
             if (jObject.ContainsKey("Duty"))
             {
                 Duty = Core.Query.IJSAMObject<SizableValue>(jObject.Value<JObject>("Duty"));
+            }
+
+            if (jObject.ContainsKey("MaximumOffcoil"))
+            {
+                MaximumOffcoil = Core.Query.IJSAMObject<ModifiableValue>(jObject.Value<JObject>("MaximumOffcoil"));
             }
 
             return result;
@@ -68,9 +90,24 @@ namespace SAM.Analytical.Systems
                 return null;
             }
 
+            if (Setpoint != null)
+            {
+                result.Add("Setpoint", Setpoint.ToJObject());
+            }
+
+            if (Efficiency != null)
+            {
+                result.Add("Efficiency", Efficiency.ToJObject());
+            }
+
             if (Duty != null)
             {
                 result.Add("Duty", Duty.ToJObject());
+            }
+
+            if (MaximumOffcoil != null)
+            {
+                result.Add("MaximumOffcoil", MaximumOffcoil.ToJObject());
             }
 
             return result;
