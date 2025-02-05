4984907c8339ad4ee50b06a2f1386fce39c2a22b
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-05 17:34:04 +0100

    ElectricalEnergySource Implemented

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/ElectricalEnergySource.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SAM.Core.Systems
+{
+    public class ElectricalEnergySource :SystemEnergySource
+    {
+        public ElectricalEnergySource(ElectricalEnergySource electricalEnergySource)
+            : base(electricalEnergySource)
+        {
+
+        }
+
+        public ElectricalEnergySource(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public ElectricalEnergySource(string name)
+            : base(name)
+        {
+
+        }
+
+        public ElectricalEnergySource(System.Guid guid, string name)
+            : base(guid, name)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            return base.FromJObject(jObject);
+        }
+
+        public override JObject ToJObject()
+        {
+            return base.ToJObject();
+        }
+    }
+}
