1a07661b07701304e9a667fd5a311e41669ec8a3
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-05 15:53:02 +0100

    SystemLoadComponentParameter added

---
+++ SAM_Systems/SAM.Analytical.Systems/Enums/Parameters/SystemLoadComponentParameter.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using SAM.Core.Attributes;
+
+namespace SAM.Analytical.Systems
+{
+    [AssociatedTypes(typeof(SystemLoadComponent)), Description("System Load Component Parameter")]
+    public enum SystemLoadComponentParameter
+    {
+        [ParameterProperties("Collection", "Collection"), SAMObjectParameterValue(typeof(CollectionLink))] Collection,
+    }
+}
