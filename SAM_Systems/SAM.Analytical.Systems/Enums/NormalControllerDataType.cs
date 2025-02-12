50a1699259e466cc5ec2caadf01ac6deddd35341
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-12 16:32:06 +0100

    NormalControllerDataType updated

--- SAM_Systems/SAM.Analytical.Systems/Enums/NormalControllerDataType.cs
+++ SAM_Systems/SAM.Analytical.Systems/Enums/NormalControllerDataType.cs
@@ -5,16 +5,18 @@ namespace SAM.Analytical.Systems
     [Description("Normal Controller Data Type")]
     public enum NormalControllerDataType
     {
-        [Description("Dry Bulb Temperature")] DryBulbTemperature = 1,
-        [Description("Humidity Ratio")] HumidityRatio = 2,
-        [Description("Relative Humidity")] RelativeHumidity = 3,
-        [Description("Enthalpy")] Enthalpy = 4,
-        [Description("Wet Bulb Temperature")] WetBulbTemperature = 5,
-        [Description("Flow")] Flow = 6,
-        [Description("Pollutant")] Pollutant = 7,
-        [Description("Pressure")] Pressure = 8,
-        [Description("Thermostat Temperature")] ThermostatTemperature = 9,
-        [Description("Humidistat Relative Humidity")] HumidistatRelativeHumidity = 10,
-        [Description("Minimal Fresh Air")] MinimalFreshAir = 11,
+        [Description("DryBulbTemperature")] DryBulbTemperature = 1,
+        [Description("Humidity Ratio")] HumidityRatio,
+        [Description("Relative Humidity")] RelativeHumidity,
+        [Description("Enthalpy")] Enthalpy,
+        [Description("Flow")] Flow,
+        [Description("Pollutant")] Pollutant,
+        [Description("Pressure")] Pressure,
+        [Description("Thermostat Temperature")] ThermostatTemperature,
+        [Description("Humidistat Relative Humidity")] HumidistatRelativeHumidity,
+        [Description("Load")] Load,
+        [Description("Wet Bulb Temperature")] WetBulbTemperature,
+        [Description("Min Flow")] MinFlow,
+        [Description("Part Load")] PartLoad,
     }
 }
