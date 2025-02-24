1dc8c6c0c20a88b551b471a22f2b948aaeb99a47
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-24 17:52:08 +0100

    CurveModifier implemented

---
+++ SAM_Systems/SAM.Analytical.Systems/Classes/Modifier/CurveModifier.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+using System.Linq;
+
+namespace SAM.Analytical.Systems
+{
+    public class CurveModifier : IndexedSimpleModifier
+    {
+        public CurveModifierType CurveModifierType { get; set; }
+        public string Name { get; set; }
+        public CurveModifierVariableType[] CurveModifierVariableTypes { get; set; }
+        public double[] Parameters { get; set; }
+
+        public CurveModifier(ArithmeticOperator arithmeticOperator, string name, CurveModifierType curveModifierType, CurveModifierVariableType[] curveModifierVariableTypes, double[] parameters)
+        {
+            ArithmeticOperator = arithmeticOperator;
+            Name = name;
+            CurveModifierType = curveModifierType;
+            CurveModifierVariableTypes = curveModifierVariableTypes?.ToArray();
+            Parameters = parameters?.ToArray();
+        }
+
+        public CurveModifier(CurveModifier curveModifier)
+            : base(curveModifier)
+        {
+            if (curveModifier != null)
+            {
+                ArithmeticOperator = curveModifier.ArithmeticOperator;
+                Name = curveModifier.Name;
+                CurveModifierType = curveModifier.CurveModifierType;
+                CurveModifierVariableTypes = curveModifier.CurveModifierVariableTypes?.ToArray();
+                Parameters = curveModifier.Parameters?.ToArray();
+            }
+        }
+
+        public CurveModifier(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            bool result = base.FromJObject(jObject);
+            if (!result)
+            {
+                return result;
+            }
+
+            if (jObject.ContainsKey("CurveModifierType"))
+            {
+                CurveModifierType = Core.Query.Enum<CurveModifierType>(jObject.Value<string>("CurveModifierType"));
+            }
+
+            if (jObject.ContainsKey("Name"))
+            {
+                Name = jObject.Value<string>("Name");
+            }
+
+            if (jObject.ContainsKey("CurveModifierVariableTypes"))
+            {
+                JArray jArray = jObject.Value<JArray>("CurveModifierVariableTypes");
+
+                CurveModifierVariableTypes = new CurveModifierVariableType[jArray.Count];
+
+                int index = 0;
+                foreach (string value in jArray)
+                {
+                    CurveModifierVariableTypes[index] = Core.Query.Enum<CurveModifierVariableType>(value);
+                    index++;
+                }
+            }
+
+            if (jObject.ContainsKey("Parameters"))
+            {
+                JArray jArray = jObject.Value<JArray>("Parameters");
+
+                Parameters = new double[jArray.Count];
+
+                int index = 0;
+                foreach (double value in jArray)
+                {
+                    Parameters[index] = value;
+                    index++;
+                }
+            }
+
+            return result;
+        }
+
+        public override JObject ToJObject()
+        {
+            JObject result = base.ToJObject();
+            if (result == null)
+            {
+                return null;
+            }
+
+            result.Add("CurveModifierType", CurveModifierType.ToString());
+
+            if(Name != null)
+            {
+                result.Add("Name", Name);
+            }
+
+            if(CurveModifierVariableTypes != null)
+            {
+                JArray jArray = new JArray();
+                foreach(CurveModifierVariableType curveModifierVariableType in CurveModifierVariableTypes)
+                {
+                    jArray.Add(curveModifierVariableType.ToString());
+                }
+
+                result.Add("CurveModifierVariableTypes", jArray);
+            }
+
+            if(Parameters != null)
+            {
+                JArray jArray = new JArray();
+                foreach (double value in Parameters)
+                {
+                    jArray.Add(value);
+                }
+
+                result.Add("Parameters", jArray);
+            }
+
+            return result;
+        }
+
+        public override bool ContainsIndex(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override double GetCalculatedValue(int index, double value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SAM_Systems/SAM.Analytical.Systems/Enums/CurveModifierType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Analytical.Systems
+{
+    [Description("Curve Modifier Type")]
+    public enum CurveModifierType
+    {
+        [Description("Linear")] Linear,
+        [Description("Bi-Linear")] BiLinear,
+        [Description("Tri-Linear")] TriLinear,
+        [Description("Quadratic")] Quadratic,
+        [Description("Bi-Quadratic")] BiQuadratic,
+        [Description("Tri-Quadratic")] TriQuadratic,
+        [Description("Cubic")] Cubic,
+        [Description("Bi-Cubic")] BiCubic,
+    }
+}
