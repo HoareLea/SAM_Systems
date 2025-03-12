fad77f042e03414464d640e05ffe3512546b190d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-03-12 15:54:42 +0100

    SystemControllerParameter added

---
+++ SAM_Systems/SAM.Analytical.Systems/Enums/Parameters/SystemControllerParameter.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using SAM.Core.Attributes;
+
+namespace SAM.Analytical.Systems
+{
+    [AssociatedTypes(typeof(ISystemController)), Description("System Controller Parameter")]
+    public enum SystemControllerParameter
+    {
+        [ParameterProperties("Group Index", "Group Index"), IntegerParameterValue()] GroupIndex,
+    }
+}
